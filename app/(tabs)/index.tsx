ac569fc7caa264b48025a5028de3309af84888a8
Jaime <176935470+HolaRene@users.noreply.github.com>
2026-03-24 10:20:56 -0600

    Agregar consulta de tareas y registro en consola en el componente Index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,14 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hook/useTheme";
+import { useQuery } from "convex/react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleTheme } = useTheme();
+
+  const tareas = useQuery(api.tareas.obtenerTareas);
+  console.log(tareas)
+
   return (
     <View    >
       <Text>Edita el archivo index.tsx. para editar esta pantalla.</Text>
