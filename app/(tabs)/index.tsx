9728be8796ac11bc2de209f53b83f1d03bcfaa3c
Jaime <176935470+HolaRene@users.noreply.github.com>
2026-03-24 17:10:17 -0600

    como agregar estilos cambiantes en inicio y un archivo de estilados para el index.tsx principal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,37 @@
-import { api } from "@/convex/_generated/api";
-import useTheme from "@/hook/useTheme";
-import { useQuery } from "convex/react";
-import { Text, TouchableOpacity, View } from "react-native";
+import useTheme, { ColorScheme } from "@/hook/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const { toggleTheme } = useTheme();
-
-  const tareas = useQuery(api.tareas.obtenerTareas);
-  console.log(tareas)
+  // Tema y estilos 
+  const { toggleTheme, colors } = useTheme();
+  const estilos = crearEstilos(colors);
 
   return (
-    <View    >
-      <Text>Edita el archivo index.tsx. para editar esta pantalla.</Text>
-      <Text>Aprende a programar</Text>
+    <View style={estilos.contenedor}>
+      <Text style={estilos.contenido}>Bienvenido a mi aplicación.</Text>
+      <Text style={estilos.contenido}>Aprende a programar</Text>
       <TouchableOpacity onPress={toggleTheme}>
-        <Text>Cambiar tema</Text>
+        <Text style={estilos.contenido}>Cambiar tema</Text>
       </TouchableOpacity>
+
     </View>
   );
 }
 
+const crearEstilos = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    contenedor: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      gap: 10,
+      backgroundColor: colors.bg
+    },
+    contenido: {
+      fontSize: 22,
+      color: colors.text,
+    }
+  })
+  return styles;
+}
 
